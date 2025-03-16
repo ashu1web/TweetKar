ed2027161579e87fc759008e9b64363f48db5e8e
ashu1web <113449680+ashu1web@users.noreply.github.com>
2025-03-16 13:54:04 +0530

    Update settings.py

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0xpo-kqg1@(f_nzegxx5h8$8^p&krnxtn=#$harrkwr5e6p%+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
