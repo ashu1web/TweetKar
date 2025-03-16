129b487f1cbfac98b308c11ff8869858c4e889e4
ashu1web <113449680+ashu1web@users.noreply.github.com>
2025-03-16 16:05:14 +0530

    Update urls.py

--- chaiheadq/chaiheadq/urls.py
+++ chaiheadq/chaiheadq/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Django Admin Panel
+    path('/admin', admin.site.urls),  # Django Admin Panel
     path('/', include('tweet.urls')),# Include tweet app URLs
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('/accounts', include('django.contrib.auth.urls')),
     
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
